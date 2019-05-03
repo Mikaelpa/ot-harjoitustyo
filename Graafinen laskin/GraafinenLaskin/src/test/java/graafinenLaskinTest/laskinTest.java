38123573915bc847c5aba3bf29ff44e9ba0579dd
Mikaelpa <palmroosmikael@gmail.com>
2019-05-03 23:49:18 +0300

    testejä

--- Graafinen laskin/GraafinenLaskin/src/test/java/graafinenLaskinTest/laskinTest.java
+++ Graafinen laskin/GraafinenLaskin/src/test/java/graafinenLaskinTest/laskinTest.java
@@ -12,14 +12,17 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import graafinenlaskin.Kayttoliittyma;
-
+import graafinenlaskin.Piirtoalusta;
+import java.awt.Graphics2D;
 
 import java.awt.event.ActionEvent;
-
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
 
 public class laskinTest {
 
     Kayttoliittyma laskin;
+    Piirtoalusta alusta;
 
     public laskinTest() {
     }
@@ -36,6 +39,7 @@ public class laskinTest {
     public void setUp() {
         laskin = new Kayttoliittyma();
         laskin.run();
+        alusta = new Piirtoalusta(0, 0, 0, 0);
     }
 
     @After
@@ -105,9 +109,58 @@ public class laskinTest {
         ActionEvent tapahtuma2 = new ActionEvent(laskin.piirraNappi, 1, laskin.piirraNappi.getActionCommand());
         laskin.actionPerformed(tapahtuma2);
         assertEquals("12", laskin.syoteKentta1.getText());
-
     }
 
+    @Test
+    public void alustaNTest() {
+        assertEquals(String.valueOf(alusta.n1), "0.0");
+        assertEquals(String.valueOf(alusta.n2), "0.0");
+        assertEquals(String.valueOf(alusta.n3), "0.0");
+        assertEquals(String.valueOf(alusta.n4), "0.0");
+    }
 
+    @Test
+    public void alustaSetNTest() {
+        alusta.setN(3.1, 4.2, 5.3, 6.4);
+        assertEquals(String.valueOf(alusta.n1), "3.1");
+        assertEquals(String.valueOf(alusta.n2), "4.2");
+        assertEquals(String.valueOf(alusta.n3), "5.3");
+        assertEquals(String.valueOf(alusta.n4), "6.4");
+    }
+
+    @Test
+    public void alustaAlkuTest() {
+        alusta.tyhjennys = true;
+        JFrame frame = new JFrame();
+        frame.add(alusta);
+        frame.pack();
+        frame.repaint();
+        assertEquals(alusta.piirtoY, 740);
+        assertTrue(alusta.tyhjennys);
+        BufferedImage bi = new BufferedImage(400, 400, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = bi.createGraphics();
+        alusta.init(g2);
+        assertEquals(alusta.piirtoY, 720);
+    }
+
+        @Test
+    public void alustaPaintTest() {
+        alusta.tyhjennys = true;
+        JFrame frame = new JFrame();
+        frame.add(alusta);
+        frame.pack();
+        frame.repaint();
+        BufferedImage bi = new BufferedImage(400, 400, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = bi.createGraphics();
+        alusta.paint(g2);
+        assertEquals(alusta.piirtoY, 720);
+    }
+    
+    @Test
+    public void alustatyhjennysTest() {
+        assertFalse(alusta.tyhjennys);
+        alusta.tyhjenna();
+        assertTrue(alusta.tyhjennys);
+    }
 
 }
